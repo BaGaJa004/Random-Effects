dd165fd39efb72e04a7f647865cd54e5eab1270a
niklasT201 <jackvcrackarchive@gmail.com>
2026-03-13 21:17:48 +0100

    updated to 1.21.11

--- src/main/java/net/bagaja/chunkeffects/KeyBindings.java
+++ src/main/java/net/bagaja/chunkeffects/KeyBindings.java
@@ -2,13 +2,13 @@ package net.bagaja.chunkeffects;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
 
     public static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(
-            ResourceLocation.fromNamespaceAndPath("chunkeffects", "general")
+            Identifier.fromNamespaceAndPath("chunkeffects", "general")
     );
 
     public static final KeyMapping OPEN_SCREEN = new KeyMapping(
